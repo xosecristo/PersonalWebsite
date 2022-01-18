f598eafb92b591e3b62b6d7950876564e05bc650
xosecristo <xosecristo@outlook.com>
2022-01-18 13:04:44 -0700

    taking out the trash

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,26 @@ const darkresume = document.querySelector('#dark-resume');
 const lightimg = document.querySelector('#light-img');
 const darkimg = document.querySelector('#dark-img');
 
+// const menu = document.querySelector('.nav-btn');
+// const nav = document.querySelector('nav');
+// function showMenu(e) {
+//     if (!nav.classList.contains('active')) {
+//         console.log("henlo");
+//         nav.classList.add('active');
+//         gsap.to('.line1', 0.25, { rotate: '45', y: 5 });
+//         gsap.to('.line2', 0.25, { rotate: '-45', y: -5 });
+//         gsap.fromTo(nav, { opacity: '0' }, { opacity: '1', duration: 0.5 });
 
-const menu = document.querySelector('.nav-btn');
-const nav = document.querySelector('nav');
-
-function showMenu(e) {
-    if (!nav.classList.contains('active')) {
-        console.log("henlo");
-        nav.classList.add('active');
-        gsap.to('.line1', 0.25, { rotate: '45', y: 5 });
-        gsap.to('.line2', 0.25, { rotate: '-45', y: -5 });
-        gsap.fromTo(nav, { opacity: '0' }, { opacity: '1', duration: 0.5 });
-
-    } else {
-        console.log("byebye");
-        gsap.to('.line1', 0.25, { rotate: '0', y: 0 });
-        gsap.to('.line2', 0.25, { rotate: '0', y: 0 });
-        gsap.fromTo(nav, { opacity: '1' }, { opacity: '0', duration: 0.5 });
-        setTimeout(
-            function () { nav.classList.remove('active') }, 500
-        );
-    }
-}
+//     } else {
+//         console.log("byebye");
+//         gsap.to('.line1', 0.25, { rotate: '0', y: 0 });
+//         gsap.to('.line2', 0.25, { rotate: '0', y: 0 });
+//         gsap.fromTo(nav, { opacity: '1' }, { opacity: '0', duration: 0.5 });
+//         setTimeout(
+//             function () { nav.classList.remove('active') }, 500
+//         );
+//     }
+// }
 
 menu.addEventListener('click', showMenu);
 
